0ec54e5ec31b40706810867e073781d589a78259
MarvelMarauder <90361775+MarvelMarauder@users.noreply.github.com>
2022-01-26 20:50:29 -0700

    Add project files.

---
+++ Mission4TaylorBullock/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4TaylorBullock.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4TaylorBullock.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext _blahContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext someName)
+        {
+            _logger = logger;
+            _blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(MovieResponse mr)
+        {
+            _blahContext.Add(mr);
+            _blahContext.SaveChanges();
+
+            return View("Confirmation", mr);
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission4TaylorBullock/Models/MovieContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4TaylorBullock.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<MovieResponse> Response { get; set;  }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieResponse>().HasData(
+                new MovieResponse
+                {
+                    MovieID = 1,
+                    Category = "Action/Adventure",
+                    Title = "The Dark Knight",
+                    Year = 2008,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13"
+                },
+                new MovieResponse
+                {
+                    MovieID = 2,
+                    Category = "Action/Adventure",
+                    Title = "The Lord of the Rings: Return of the King",
+                    Year = 2003,
+                    Director = "Peter Jackson",
+                    Rating = "PG-13"
+                },
+                new MovieResponse
+                {
+                    MovieID = 3,
+                    Category = "Comedy",
+                    Title = "Monty Python and the Holy Grail",
+                    Year = 1975,
+                    Director = "Terry Gilliam, Terry Jones",
+                    Rating = "PG"
+                });
+        }
+    }
+}
